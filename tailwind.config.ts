ff7b95f00451e4cb012d1c031d9a18f18535892e
iphonewarriorsmail <iphonewarriorsmail@gmail.com>
2026-04-20 03:56:48 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // ACTIVAR EL MODO OSCURO POR CLASE
   darkMode: 'class', 
-  
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };

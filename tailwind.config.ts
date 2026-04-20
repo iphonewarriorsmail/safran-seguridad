f5d93074ef5188a5f223750414b63bf3872406b2
iphonewarriorsmail <iphonewarriorsmail@gmail.com>
2026-04-20 03:38:37 -0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config {
+  // 1. ACTIVAR EL MODO OSCURO POR CLASE
+  darkMode: 'class', 
+  
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;

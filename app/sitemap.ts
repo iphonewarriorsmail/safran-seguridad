e5d99d406cf427c31c518e25057bc59b1e8cec81
iphonewarriorsmail <iphonewarriorsmail@gmail.com>
2026-04-22 11:03:41 -0300

    Comment out blogEntries mapping in sitemap
    Commented out the blogEntries mapping and related code.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,16 @@
-/*import { MetadataRoute } from 'next';
-import { blogPosts } from './lib/blog-data';
+import { MetadataRoute } from 'next';
+/*import { blogPosts } from './lib/blog-data';*/
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://safraseguridad.com.ar'; // Reemplazar con la URL real si es distinta
 
-  const blogEntries: MetadataRoute.Sitemap = blogPosts.map((post) => ({
+  /*const blogEntries: MetadataRoute.Sitemap = blogPosts.map((post) => ({
     url: `${baseUrl}/blog/${post.id}`,
     lastModified: new Date(),
     changeFrequency: 'weekly',
     priority: 0.7,
   }));
-
+*/
   return [
     {
       url: baseUrl,
@@ -21,4 +21,3 @@ export default function sitemap(): MetadataRoute.Sitemap {
     ...blogEntries,
   ];
 }
-*/
